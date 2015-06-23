ab559613596ba38dd10de19d6e42a655f55e0634
123456789aaaaa <s1520635@u.tsukuba.ac.jp>
2015-06-23 17:51:54 +0900

    UI変更，bluetoothの各種設定（使用可能かどうかの確認，bluetoothの接続要求など）温度センサー設定（途中）

--- buttonc/app/src/main/java/com/example/aa/buttonc/MainActivity.java
+++ buttonc/app/src/main/java/com/example/aa/buttonc/MainActivity.java
@@ -2,14 +2,32 @@ package com.example.aa.buttonc;
 
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Set;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
+    private static final int REQUEST_ENABLE_BLUETOOTH =10 ;
+    private SensorManager Smanager;
+    private TextView text;
+    //BluetoothAdapter取得
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +42,52 @@ public class MainActivity extends Activity implements View.OnClickListener {
         right.setOnClickListener(this);
         Button up = (Button)findViewById(R.id.up);
         up.setOnClickListener(this);
+
+        TextView text = (TextView)findViewById(R.id.temperature);
+
+
+        Smanager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        //List<Sensor> sensors = Smanager.getSensorList(Sensor.TYPE_LIGHT);
+
+
+
+        BluetoothAdapter Bt = BluetoothAdapter.getDefaultAdapter();
+        if(Bt!=null){
+            //Bluetooth対応端末の場合の処理
+            Log.d("Bluetoothがサポートされてます。","a"+Bt);
+        }else{
+            //Bluetooth非対応端末の場合の処理
+            Log.d("Bluetoothがサポートされていません。","a"+Bt);
+
+        }
+        boolean btEnable = Bt.isEnabled();
+        if(btEnable == true){
+            //BluetoothがONだった場合の処理
+        }else{
+            //OFFだった場合、ONにすることを促すダイアログを表示する画面に遷移
+            Intent btOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(btOn, REQUEST_ENABLE_BLUETOOTH);
+        }
+
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        int light_value = (int)(event.values[0]);
+        text.setText("明るすぎて眼が痛いにゃ。"+light_value);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int ResultCode, Intent date){
+        if(requestCode == REQUEST_ENABLE_BLUETOOTH){
+            if(ResultCode == Activity.RESULT_OK){
+                //BluetoothがONにされた場合の処理
+                Log.d("BluetoothをONにしてもらえました。","a"+1);
+            }else{
+                Log.d("BluetoothがONにしてもらえませんでした。","a"+2);
+                finish();
+            }
+        }
     }
 
 
